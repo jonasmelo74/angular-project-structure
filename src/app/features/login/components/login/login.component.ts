6b4e72a4e5b061039feb5a852c2da7eb7fc55d2c
Jonas Melo da Paz <jonas.melo@essentia.com.br>
2025-03-18 09:47:42 -0300

    adicionando components

--- src/app/features/login/components/login/login.component.ts
+++ src/app/features/login/components/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NgSelectModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  cities = [
+    { id: 1, name: 'New York' },
+    { id: 2, name: 'Los Angeles' },
+    { id: 3, name: 'Chicago' }
+  ];
 }
