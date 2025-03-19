1ece400c19eb9151ec0b5592f79a2add2eded489
Jonas Melo da Paz <jonas.melo@essentia.com.br>
2025-03-19 17:08:18 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './features/login/components/login/login.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { AsideComponent } from './shared/components/aside/aside.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, NavbarComponent],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, AsideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
